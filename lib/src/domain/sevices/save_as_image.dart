5cb891ef1c96ff95eac9af210806c1c958601511
jeremybrehezx <jeremy.brehe@zoxs.de>
2025-02-20 22:05:25 +0100

    Replace image_gallery_saver with gal package for image saving

--- lib/src/domain/sevices/save_as_image.dart
+++ lib/src/domain/sevices/save_as_image.dart
@@ -1,45 +1,48 @@
+import 'dart:developer';
 import 'dart:io';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:gal/gal.dart';
 import 'package:path_provider/path_provider.dart';
 
-Future takePicture(
-    {required contentKey,
-    required BuildContext context,
-    required saveToGallery,
-    required fileName,}) async {
+Future takePicture({
+  required GlobalKey contentKey,
+  required BuildContext context,
+  required bool saveToGallery,
+  required String fileName,
+}) async {
   try {
-    /// converter widget to image
+    /// Convert widget to image
     final RenderRepaintBoundary boundary =
-        contentKey.currentContext.findRenderObject();
-
+        contentKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
     final ui.Image image = await boundary.toImage(pixelRatio: 3);
-
-    final ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    final ByteData? byteData =
+        await image.toByteData(format: ui.ImageByteFormat.png);
     final Uint8List pngBytes = byteData!.buffer.asUint8List();
 
-    /// create file
+    /// Create file in app's directory
     final String dir = (await getApplicationDocumentsDirectory()).path;
-    final String imagePath = '$dir/${fileName}_${DateTime.now()}.png';
+    final String imagePath =
+        '$dir/${fileName}_${DateTime.now().millisecondsSinceEpoch}.png';
     final File capturedFile = File(imagePath);
     await capturedFile.writeAsBytes(pngBytes);
 
     if (saveToGallery) {
-      final result = await ImageGallerySaver.saveImage(pngBytes,
-          quality: 100, name: '${fileName}_${DateTime.now()}.png',);
-      if (result != null) {
+      try {
+        await Gal.putImage(imagePath);
         return true;
-      } else {
+      } on GalException catch (e) {
+        log(e.toString());
         return false;
       }
     } else {
       return imagePath;
     }
   } catch (e) {
-    debugPrint('exception => $e');
+    debugPrint('Exception: $e');
     return false;
   }
 }
